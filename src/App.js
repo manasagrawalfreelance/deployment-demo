ff85757f3413d2fa61e6cce7b672c79f47047395
Manas Agrawal <manasagrawal.freelance@gmail.com>
2021-04-18 13:45:01 +0530

    added firebase connectivity

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fire from './fire';
+class App extends Component{
+  state={
+    text:""
+  }
+  handleText=e=>{
+    this.setState({
+      text:e.target.value
+    })
+  }
+  handleSubmit=e=>{
+    let messageRef=fire.database().ref('messages').orderByKey().limitToLast(100);
+    fire.database().ref('messages').push(this.state.text);
+    this.setState({
+      text:""
+    })
+  }
 
-function App() {
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello React-App Manas</h1>
+      <input type="text" onChange={this.handleText} id="txtName"/>
+      <br />
+      <button id="btnSave" onClick={this.handleSubmit}>Save</button>
       </header>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/fire.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+var firebaseConfig = {
+    apiKey: "AIzaSyBVRRGVg9FB7bZmTDikLp3eD_ON22xeEh0",
+    authDomain: "react-firebase-6cd84.firebaseapp.com",
+    projectId: "react-firebase-6cd84",
+    storageBucket: "react-firebase-6cd84.appspot.com",
+    messagingSenderId: "1098416676502",
+    appId: "1:1098416676502:web:168f61b46c2b8ad79d2cb7",
+    measurementId: "G-VECVL2NV8R"
+  };
+  var fire=firebase.initializeApp(firebaseConfig);
+  export default fire;
